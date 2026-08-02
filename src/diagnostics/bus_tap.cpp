c4ee16828cd01a166b2dca94914eb8d1a798d80a
Tim de Bruijn <tim@timdebruijn.nl>
2026-08-02 16:51:37 +0200

    Revert "TEMPORARY: deliberately broken, to prove the cache cannot hide it"
    This reverts commit 61dd823c4d8dd5c5297edb34712f4f6203829394.

--- src/diagnostics/bus_tap.cpp
+++ src/diagnostics/bus_tap.cpp
@@ -9,8 +9,8 @@ namespace heliograph::diag {
 
 const char* busDirectionName(BusDirection direction) {
     switch (direction) {
-        case BusDirection::Tx: return "rx";
-        case BusDirection::Rx: return "tx";
+        case BusDirection::Tx: return "tx";
+        case BusDirection::Rx: return "rx";
     }
     return "unknown";
 }
